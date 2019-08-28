87d13388e1c6b7c5054aad3103f6f740fcb64b97
vkomolov <vkomolov1974@gmail.com>
2019-08-29 01:39:23 +0300

    to set the gh-pages

---
+++ precache-manifest.cd51aab020639f93f36dba157dd25527.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389240f0a728197f4c18cc25c0ffb8e6",
+    "url": "/cv-app/index.html"
+  },
+  {
+    "revision": "728e8049dae1b31561a9",
+    "url": "/cv-app/static/css/main.66e0f621.chunk.css"
+  },
+  {
+    "revision": "062c9fe463d97b076a03",
+    "url": "/cv-app/static/js/2.25fd421b.chunk.js"
+  },
+  {
+    "revision": "728e8049dae1b31561a9",
+    "url": "/cv-app/static/js/main.d993bd46.chunk.js"
+  },
+  {
+    "revision": "f8e0dec55cc7f0903dc7",
+    "url": "/cv-app/static/js/runtime~main.1f72d373.js"
+  }
+]);
