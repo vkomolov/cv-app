5742b8b52c9c83ced25d0029d04dd0b7436274e5
vkomolov <vkomolov1974@gmail.com>
2019-12-04 19:02:57 +0200

    to update the data

---
+++ precache-manifest.dcbe7d787d73531abdb12d8867d4d9c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63eed766c83bac650c9534eedd83e6bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b83f4af4544434e11343",
+    "url": "/static/css/main.4c31e7b5.chunk.css"
+  },
+  {
+    "revision": "24a1f113f986f77bea7b",
+    "url": "/static/js/2.3a9dd670.chunk.js"
+  },
+  {
+    "revision": "b83f4af4544434e11343",
+    "url": "/static/js/main.6c8c4c7b.chunk.js"
+  },
+  {
+    "revision": "817666a7337e23ff1ef6",
+    "url": "/static/js/runtime-main.54692e8e.js"
+  }
+]);
