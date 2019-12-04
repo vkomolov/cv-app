07d7a580adc6abaff4276617df5ecc5a4d9573f4
vkomolov <vkomolov1974@gmail.com>
2019-12-04 21:57:18 +0200

    to place build

---
+++ precache-manifest.81c9ad1deafaf833c74a5c115ffb3790.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17a4d0a9728253f45a2144cc20e76c1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "59c2fe812d073b30f397",
+    "url": "./static/css/main.03e75189.chunk.css"
+  },
+  {
+    "revision": "24a1f113f986f77bea7b",
+    "url": "./static/js/2.3a9dd670.chunk.js"
+  },
+  {
+    "revision": "59c2fe812d073b30f397",
+    "url": "./static/js/main.00a063aa.chunk.js"
+  },
+  {
+    "revision": "163bc4a10d2dda4504da",
+    "url": "./static/js/runtime-main.14a94163.js"
+  }
+]);
